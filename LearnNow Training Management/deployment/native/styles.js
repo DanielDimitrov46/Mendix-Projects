06bea7fb0d6d5b2241af51b998e0e88fef6ffd16
daniel <dani.dimitrov.snack9@gmail.com>
2023-11-08 00:54:12 +0200

    first Mendix program

---
+++ LearnNow Training Management/deployment/native/styles.js
@@ -0,0 +1,11 @@
+import * as style0 from "D:/UKTC PROGRAMMING/Mendix-low-code-Course-System/LearnNow Training Management/themesource/administration/native/main";
+import * as style1 from "D:/UKTC PROGRAMMING/Mendix-low-code-Course-System/LearnNow Training Management/themesource/nanoflowcommons/native/main";
+import * as style2 from "D:/UKTC PROGRAMMING/Mendix-low-code-Course-System/LearnNow Training Management/themesource/webactions/native/main";
+import * as style3 from "D:/UKTC PROGRAMMING/Mendix-low-code-Course-System/LearnNow Training Management/themesource/atlas_core/native/main";
+import * as style4 from "D:/UKTC PROGRAMMING/Mendix-low-code-Course-System/LearnNow Training Management/themesource/feedbackmodule/native/main";
+import * as style5 from "D:/UKTC PROGRAMMING/Mendix-low-code-Course-System/LearnNow Training Management/themesource/myfirstmodule/native/main";
+import * as style6 from "D:/UKTC PROGRAMMING/Mendix-low-code-Course-System/LearnNow Training Management/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5, style6]);
